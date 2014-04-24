b018406e4267990d34b88ae08ada321817e44245
HDemirci <huse-yin@hotmail.de>
2014-04-24 13:48:23 +0200

    some stuff

--- conf.lua
+++ conf.lua
@@ -1,12 +1,12 @@
 function love.conf(t)
 	t.title = "Project Umbrella"
-	t.window.width = 1024               -- The window width (number)
-    t.window.height = 768              -- The window height (number)
+	t.window.width = 800              -- The window width (number)
+    t.window.height = 600              -- The window height (number)
     t.window.borderless = true        -- Remove all border visuals from the window (boolean)
     t.window.resizable = true         -- Let the window be user-resizable (boolean)
     t.window.minwidth = 1              -- Minimum window width if the window is resizable (number)
     t.window.minheight = 1             -- Minimum window height if the window is resizable (number)
     t.window.fullscreen = false       -- Enable fullscreen (boolean)
     t.window.fullscreentype = "normal" -- Standard fullscreen or desktop fullscreen mode (string)
-		 t.window.vsync = true
+	t.window.vsync = true
 end
